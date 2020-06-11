82d7cc5ff054c7a05acdd19efc000bb6ae4351fb
waleedfarraj <waleed.farraj95@gmail.com>
2020-06-11 14:26:31 +0300

    code-3 pass fix

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -50,11 +50,11 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-return[a+b,a*b, '4 and 7 and 5 sum to 16.','The product of 4 and 7 and 5 is 140.']
+return[a+b+c,a*b*c, '4 and 7 and 5 sum to 16.','The product of 4 and 7 and 5 is 140.']
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
