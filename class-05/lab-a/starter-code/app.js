91717275755b392dadc903ecb2d58e56ac8d47ef
waleedfarraj <waleed.farraj95@gmail.com>
2020-06-11 14:11:51 +0300

    code-1 pass

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -9,9 +9,9 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-var x
-x = a+b
-return(X)
+
+return[a+b,'The sum of 4 and 7 is 11.']
+
 }
 
 // Here is the test for sum(); uncomment it to run it
